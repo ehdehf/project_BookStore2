eabd4d333bf4a0d8c9dd2b9c09c316139dcfa5ce
swj6498 <119040020+swj6498@users.noreply.github.com>
2025-11-18 14:46:50 +0900

    Update OrderRequestDTO.java

--- boot_book_store/src/main/java/com/boot/dto/OrderRequestDTO.java
+++ boot_book_store/src/main/java/com/boot/dto/OrderRequestDTO.java
@@ -1,13 +1,20 @@
 package com.boot.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequestDTO {
     private List<OrderItemDto> items;
 
     @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class OrderItemDto {
         private int book_id;
         private int quantity;
